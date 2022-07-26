82e0e4c41f465c2983630d96295e2c71754ae020
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2022-07-26 12:00:26 -0300

    Interligando as classes de domínio

--- src/com/github/wesleyav/desafio/dominio/Main.java
+++ src/com/github/wesleyav/desafio/dominio/Main.java
@@ -24,9 +24,42 @@ public class Main {
 		mentoria.setDescricao("descrição mentoria java");
 		mentoria.setData(LocalDate.now());
 
-		System.out.println(curso1);
-		System.out.println(curso2);
-		System.out.println(mentoria);
+//		System.out.println(curso1);
+//		System.out.println(curso2);
+//		System.out.println(mentoria);
+
+		Bootcamp bootcamp = new Bootcamp();
+		bootcamp.setNome("Bootcamp Java Developer");
+		bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+		bootcamp.getConteudos().add(curso1);
+		bootcamp.getConteudos().add(curso2);
+		bootcamp.getConteudos().add(mentoria);
+
+		Dev dev1 = new Dev();
+		dev1.setNome("Dev 1");
+		dev1.inscreverBootcamp(bootcamp);
+		System.out.println("Conteúdos Inscritos de Dev1" + dev1.getConteudosInscritos());
+		dev1.progredir();
+		dev1.progredir();
+		System.out.println("-");
+		System.out.println("Conteúdos Inscritos de Dev1" + dev1.getConteudosInscritos());
+		System.out.println("Conteúdos Concluidos de Dev1" + dev1.getConteudosConcluidos());
+		System.out.println("XP:" + dev1.calcularTotalXP());
+
+		System.out.println("---------------");
+
+		Dev dev2 = new Dev();
+		dev2.setNome("Dev 2");
+		dev2.inscreverBootcamp(bootcamp);
+		System.out.println("Conteúdos Inscritos de Dev2" + dev2.getConteudosInscritos());
+		dev2.progredir();
+		dev2.progredir();
+		dev2.progredir();
+		System.out.println("-");
+		System.out.println("Conteúdos Inscritos de Dev2" + dev2.getConteudosInscritos());
+		System.out.println("Conteúdos Concluidos de Dev2" + dev2.getConteudosConcluidos());
+		System.out.println("XP:" + dev2.calcularTotalXP());
+
 	}
 
 }

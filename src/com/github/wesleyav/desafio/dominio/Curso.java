17b85775ff3271f9ec70d9ddcfef3352900ac16e
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2022-07-26 11:13:27 -0300

    Herança e Polimorfismo - parte 1

--- src/com/github/wesleyav/desafio/dominio/Curso.java
+++ src/com/github/wesleyav/desafio/dominio/Curso.java
@@ -1,28 +1,10 @@
 package com.github.wesleyav.desafio.dominio;
 
-public class Curso {
+public class Curso extends Conteudo {
 
-	private String titulo;
-	private String descricao;
 	private int cargaHoraria;
 
 	public Curso() {
-	}
-
-	public String getTitulo() {
-		return titulo;
-	}
-
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-
-	public String getDescricao() {
-		return descricao;
-	}
-
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
 	}
 
 	public int getCargaHoraria() {
@@ -35,7 +17,13 @@ public class Curso {
 
 	@Override
 	public String toString() {
-		return "Curso [titulo=" + titulo + ", descricao=" + descricao + ", cargaHoraria=" + cargaHoraria + "]";
+		return "Curso [titulo=" + getTitulo() + ", descricao=" + getDescricao() + ", cargaHoraria=" + cargaHoraria
+				+ "]";
+	}
+
+	@Override
+	public double calcularXP() {
+		return XP_PADRAO * cargaHoraria;
 	}
 
 }
--- src/com/github/wesleyav/desafio/dominio/Mentoria.java
+++ src/com/github/wesleyav/desafio/dominio/Mentoria.java
@@ -2,29 +2,11 @@ package com.github.wesleyav.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
+public class Mentoria extends Conteudo {
 
-	private String titulo;
-	private String descricao;
 	private LocalDate data;
 
 	public Mentoria() {
-	}
-
-	public String getTitulo() {
-		return titulo;
-	}
-
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-
-	public String getDescricao() {
-		return descricao;
-	}
-
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
 	}
 
 	public LocalDate getData() {
@@ -37,7 +19,12 @@ public class Mentoria {
 
 	@Override
 	public String toString() {
-		return "Mentoria [titulo=" + titulo + ", descricao=" + descricao + ", data=" + data + "]";
+		return "Mentoria [titulo=" + getTitulo() + ", descricao=" + getDescricao() + ", data=" + data + "]";
+	}
+
+	@Override
+	public double calcularXP() {
+		return XP_PADRAO + 20;
 	}
 
 }
